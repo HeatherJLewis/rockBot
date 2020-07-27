97fb6bd1f25317a2e8c4d8910288c22594a22320
Heather Lewis <heather.lewis@bbc.co.uk>
2020-07-27 13:36:12 +0100

    Adds check for a tie in the previous round then plays waterbomb: worse for P1

--- myBot1.js
+++ myBot1.js
@@ -1,17 +1,46 @@
+
 class Bot {
     makeMove(gamestate) {
-        let runningTotals = {
+        let dynamiteCount = 0;
+
+        gamestate.rounds.forEach(round => {
+            return round.p1 ==='D' ? dynamiteCount += 1 : dynamiteCount;
+        });
+
+        let runningTotalsP1 = {
+            R: 0,
+            P: 0,
+            S: 0,
+            D: 0,
+            W: 0,
+            isLastRoundTie : false,
+
+        };
+
+        let runningTotalsP2 = {
             R: 0,
             P: 0,
             S: 0,
             D: 0,
             W: 0
-        }
+        };
 
         gamestate.rounds.forEach(round => {
-            runningTotals[round.p1] += 1
-        })
-        console.log(runningTotals);
+            runningTotalsP1[round.p1] += 1
+        });
+
+        gamestate.rounds.forEach(round => {
+            runningTotalsP2[round.p2] += 1
+        });
+
+        const lastMove = gamestate.rounds[(gamestate.rounds.length)-1];
+
+        if(gamestate.rounds.length > 0 && dynamiteCount < 100){
+            if(lastMove.p1 === lastMove.p2){
+                return 'D';
+            }
+        }
+
         const randomFactor = Math.random()*10;
 
         if(randomFactor <= 3){
@@ -20,11 +49,11 @@ class Bot {
             return 'P';
         } else if(randomFactor > 6 && randomFactor <= 9){
             return 'S';
-        } else if(randomFactor > 9 && randomFactor <= 9.7 && runningTotals.D < 100){
+        } else if(randomFactor > 9 && randomFactor <= 9.7 && dynamiteCount < 100){
             return 'D';
         } else {
             return 'W';
-        }
+        };
     }
 }
 
