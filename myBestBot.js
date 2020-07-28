59352a715abf79a94e3cb12b5b80a1123b9bec8a
Heather Lewis <heather.lewis@bbc.co.uk>
2020-07-28 13:58:07 +0100

    Adds test for opponents treatment of ties

--- myBestBot.js
+++ myBestBot.js
@@ -16,10 +16,15 @@ class Bot {
         }
         this.numberOfgamesPlayed = 0;
         this.lastMove = {};
+        this.ties = {
+            numberOfDynamites: 0
+        }
     }
 
     updateRunningTotals(gamestate){
         this.lastMove = gamestate.rounds[(gamestate.rounds.length)-1];
+        this.secondToLastMove = gamestate.rounds[(gamestate.rounds.length)-2];
+
         if(this.lastMove != undefined){
             this.runningTotalP1[this.lastMove.p1]++;
         }
@@ -28,6 +33,10 @@ class Bot {
             this.runningTotalP2[this.lastMove.p2]++;
         }
         this.numberOfgamesPlayed++;
+
+        if(this.lastMove != undefined && this.secondToLastMove != undefined && this.secondToLastMove.p1 === this.secondToLastMove.p2 && this.lastMove.p2 === 'D'){
+            this.ties.numberOfDynamites++;
+        }
     }
 
     generateRandomPlay(){
@@ -37,32 +46,33 @@ class Bot {
             return 'R';
         } else if(randomFactor > 3 && randomFactor <= 6){
             return 'P';
-        } else if(randomFactor > 6 && randomFactor <= 9){
-            return 'S';
-        } else if(randomFactor > 9 && randomFactor <= 9.7 && this.runningTotalP1.D < 100){
-            return 'D';
         } else {
-            return 'W';
-        };
+            return 'S';
+        }
     };
+
+    checkForDynamiteAfterATie(){
+
+    }
 
     generateResponsivePlay(){
         const randomFactor = Math.random();
         const numberofRPS = this.runningTotalP2.R+this.runningTotalP2.P+this.runningTotalP2.S;
         if(randomFactor <= this.runningTotalP2.R/numberofRPS){
             return 'P';
-        } else if(randomFactor > this.runningTotalP2.R/this.numberOfgamesPlayed && randomFactor <= (this.runningTotalP2.R+this.runningTotalP2.P)/numberofRPS){
+        } else if(randomFactor > this.runningTotalP2.R/numberofRPS && randomFactor <= (this.runningTotalP2.R+this.runningTotalP2.P)/numberofRPS){
             return 'S';
         } else {
             return 'R';
         }
-
     };
 
     makeMove(gamestate){
         this.updateRunningTotals(gamestate);
+
+        (this.numberOfgamesPlayed < 200) && console.log(this.numberOfgamesPlayed, this.lastMove, this.ties.numberOfDynamites);
+
         if(this.numberOfgamesPlayed < 50){
-            console.log(this.runningTotalP2);
             return this.generateRandomPlay();
         }
 
@@ -78,14 +88,17 @@ class Bot {
             return 'R'
         }
 
-        if(this.numberOfgamesPlayed > 1 && this.runningTotalP2.D < 100 && this.lastMove.p1 === this.lastMove.p2){
+        if(this.numberOfgamesPlayed > 1 && this.runningTotalP2.D < 100 && this.lastMove.p1 === this.lastMove.p2 && this.ties.numberOfDynamites < 20){
+            return 'R';
+        }
+
+        if(this.numberOfgamesPlayed > 1 && this.runningTotalP2.D < 100 && this.lastMove.p1 === this.lastMove.p2 && this.ties.numberOfDynamites >= 20){
             return 'W';
         }
 
         if(this.numberOfgamesPlayed > 1 && this.runningTotalP1.D < 100 && this.lastMove.p1 === this.lastMove.p2){
             return 'D';
         }
-
 
         return this.generateResponsivePlay();
     }
---
+++ testBotRandomSkewedHandlesDynamite.js
@@ -0,0 +1,61 @@
+class Bot {
+    constructor(){
+        this.runningTotalP1 = {
+            R: 0,
+            P: 0,
+            S: 0,
+            D: 0,
+            W: 0
+        };
+        this.runningTotalP2 = {
+            R: 0,
+            P: 0,
+            S: 0,
+            D: 0,
+            W: 0
+        }
+        this.numberOfgamesPlayed = 0;
+        this.lastMove = {};
+    }
+
+    updateRunningTotals(gamestate){
+        this.lastMove = gamestate.rounds[(gamestate.rounds.length)-1];
+        if(this.lastMove != undefined){
+            this.runningTotalP1[this.lastMove.p1]++;
+        }
+
+        if(this.lastMove != undefined){
+            this.runningTotalP2[this.lastMove.p2]++;
+        }
+        this.numberOfgamesPlayed++;
+    }
+
+    generateRandomPlay(){
+        const randomFactor = Math.random()*10;
+
+        if(randomFactor <= 3){
+            return 'R';
+        } else if(randomFactor > 3 && randomFactor <= 6){
+            return 'P';
+        } else if(randomFactor > 6 && randomFactor <= 9){
+            return 'S';
+        } else if(randomFactor > 9 && randomFactor <= 9.7 && this.runningTotalP1.D < 100){
+            return 'D';
+        } else {
+            return 'W';
+        };
+    };
+
+    makeMove(gamestate){
+        this.updateRunningTotals(gamestate);
+
+        if(this.numberOfgamesPlayed > 1 && this.runningTotalP1.D < 100 && this.lastMove.p1 === this.lastMove.p2){
+            return 'D';
+        }
+
+        return this.generateRandomPlay();
+
+    }
+}
+
+module.exports = new Bot();
